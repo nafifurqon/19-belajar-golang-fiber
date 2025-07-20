aa46400bd78a321fc2c76a9db6c63460a272fff6
Muhammad Nafi' Furqon Diani <muhammad.diani@electrum.id>
2025-07-20 17:42:49 +0700

    4. Routinfg - init

--- main.go
+++ main.go
@@ -7,13 +7,17 @@ import (
 )
 
 func main() {
-	fiber := fiber.New(fiber.Config{
+	app := fiber.New(fiber.Config{
 		IdleTimeout:  time.Second * 5,
 		ReadTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 	})
 
-	err := fiber.Listen("localhost:3000")
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	err := app.Listen("localhost:3000")
 	if err != nil {
 		panic(err)
 	}
