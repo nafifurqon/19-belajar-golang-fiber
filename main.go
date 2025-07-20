bdfa35e26e5765977bb81b90c80fca3f5075b3cd
Muhammad Nafi' Furqon Diani <muhammad.diani@electrum.id>
2025-07-20 17:25:40 +0700

    3. Configuration - init

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fiber := fiber.New()
+	fiber := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+	})
 
 	err := fiber.Listen("localhost:3000")
 	if err != nil {
